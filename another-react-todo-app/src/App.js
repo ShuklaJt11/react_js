416ae5c1ba3ecd8e4ae691dd8845b181c78ba2ff
Jatin Shukla <jatinshukla.lek@gmail.com>
2020-09-25 15:01:49 +0530

    Todo App progress

--- another-react-todo-app/src/App.js
+++ another-react-todo-app/src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react'
 import TodoComponent from './TodoComponent'
-import todos from './ToDoList'
+import todosData from './ToDoList'
 
-function App() {
-  const todoComponents = todos.map(todo => <TodoComponent key={todo.id} todoData={todo} />)
-  return (
-    <div className="app-container">
-      <h1>ToDo List!</h1>
-      <div className="todo-container">
-        {todoComponents}
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleCheck = this.handleCheck.bind(this)
+  }
+
+  handleCheck(id) {
+    console.log('changing', id)
+    this.setState(prevState => {
+      return { 
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed
+            }
+          }
+          return todo
+        })
+      }
+    })
+  }
+
+  render() {
+    const todoComponents = this.state.todos.map(todo => <TodoComponent key={todo.id} todoData={todo} checkFunction={this.handleCheck} />)
+    return (
+      <div className="app-container">
+        <h1>ToDo List!</h1>
+        <div className="todo-container">
+          {todoComponents}
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
--- another-react-todo-app/src/TodoComponent.js
+++ another-react-todo-app/src/TodoComponent.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function TodoComponent(props) {
+class TodoComponent extends Component {
+  render() {
     return (
-        <div className='todo-object'>
-            <input className="todo-checkbox" type="checkbox" checked={props.todoData.completed} />
-            <p className="todo-label">{props.todoData.text}</p>
-        </div>
+      <div className='todo-object'>
+        <input className="todo-checkbox" type="checkbox" checked={this.props.todoData.completed} onChange={() => this.props.checkFunction(this.props.todoData.id)} />
+        <p className="todo-label">{this.props.todoData.text}</p>
+      </div>
     )
+  }
 }
 
 export default TodoComponent
